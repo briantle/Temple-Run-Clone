ab5870ab53b44992d218db87a6d4b1d45e1aeba4
briantle <31326217+briantle@users.noreply.github.com>
2018-08-28 15:10:06 -0500

    Set up player script to implement animations. However, need to find a good character and test it

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour 
 {
 	private CharacterController controller;
-	public bool isDead = false;
+	private Rigidbody[] bodies;
 	public float movementSpeed;
 	// Used to apply gravity to our character
 	public float verticalVelocity = 0.0f;
+	public float gravity = 12f;
+	public Animation anim;
 
 	// Use this for initialization
 	void Start () {
+		anim = GetComponent<Animation> ();
+		bodies = GetComponentsInChildren<Rigidbody> ();
+		setKinematic (true);
 		controller = gameObject.GetComponent<CharacterController> ();
 		// if forgot to set a value for the speed, default it to 5
 		if (movementSpeed == 0)
@@ -21,6 +26,8 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		if (!anim.isPlaying)
+			anim.Play ();
 		Vector3 moveVector = Vector3.zero;
 		// If our player is on the ground, then reset the vertical velocity
 		if (controller.isGrounded)
@@ -28,22 +35,29 @@ public class Player : MonoBehaviour
 		// Otherwise, we want to apply gravity to make our character fall down.
 		// We are going to be using the gravity set in our project settings
 		else {
-			verticalVelocity -= Physics.gravity.y * Time.deltaTime;
+			verticalVelocity -= gravity * Time.deltaTime;
 			// This is going to check if our player falls out of bounds
 			RaycastHit hit;
 			// If our character falls down and there isn't an object below him, he's dead
 			if (!Physics.Raycast (transform.position, -Vector3.up, out hit)) {
 				Debug.Log ("Player has fallen out of bounds");
-				isDead = true;
+				// Ragdoll physics
+				setKinematic (false);
+				controller.enabled = false;
 			}
 		}
 		// Applies gravity to player
 		moveVector.y = verticalVelocity;
 		// Player can strafe left or right
-		moveVector.x = Input.GetAxisRaw("Horizontal") * movementSpeed;
+		moveVector.x = Input.GetAxisRaw ("Horizontal") * movementSpeed;
 		// Player's forward movement
 		moveVector.z = movementSpeed;
 		// Player will constantly move forward
 		controller.Move (moveVector * Time.deltaTime);
+	}
+	public void setKinematic(bool isKinematic)
+	{
+		foreach (Rigidbody rb in bodies)
+			rb.isKinematic = isKinematic;
 	}
 }
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Temple-Run-Clone
 
 Assets Used:
-
-https://assetstore.unity.com/packages/3d/characters/creatures/character-monster-1-76640
