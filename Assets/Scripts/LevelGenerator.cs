4f1438b920b3d57aa1bbb60dda82ba30e2d2255d
briantle <lebriantu@gmail.com>
2018-09-07 11:35:10 -0500

    created variations for the platform and changed values for how far the platforms will spawn

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour {
 
 	public float distanceToInstantiate = 100f;
-	public GameObject platform;
+    public GameObject[] platforms;
 	public GameObject wallTrap;
 	private Vector3 currPosition = Vector3.zero;
 
 	void Update(){
 		Transform trans = gameObject.transform;
-		if (Vector3.Distance(Camera.main.transform.position, currPosition) < distanceToInstantiate){
-				Instantiate(platform, currPosition, platform.transform.rotation, gameObject.transform);
-				currPosition += new Vector3(Random.Range(-2, 3), Random.Range(-1, 2), Random.Range(9, 13));
+		if (Vector3.Distance(Camera.main.transform.position, currPosition) < distanceToInstantiate)
+        {
+                //int i = Random.Range(0, platforms.Length);
+                Instantiate(platforms[Random.Range(0, platforms.Length)], currPosition, platforms[0].transform.rotation, gameObject.transform);
+				currPosition += new Vector3(Random.Range(-2, 4), Random.Range(-3, 2), Random.Range(6, 13));
 		}
 	}
 }
--- Assets/Scripts/ShowTime.cs
+++ Assets/Scripts/ShowTime.cs
@@ -5,16 +5,24 @@ using UnityEngine.UI;
 public class ShowTime : MonoBehaviour {
 
 	public Text timeText;
+    public Text fpsText;
+    private float deltaTime;
 	public float finalTime = 0f;
-	public Player playa;
+	private Player playa;
 	
 	void Start(){
 		playa = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (playa.enabled){
+	void Update ()
+    {
+        // Shows Framerate, this is to know how our game is performing
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = Mathf.Ceil(fps).ToString();
+        // As long as the player is alive, we want to keep a timer of how longer they have been running
+        if (playa.enabled){
 			timeText.text = "Time: " + (int) Time.realtimeSinceStartup;
 			finalTime = Time.realtimeSinceStartup;
 		}
